b15ab7c5d6f3c8a4489042d9dcc6629d6e564468
Tapan Shah <tapan15in@gmail.com>
2016-06-06 15:56:40 +0000

    Enable fix boot patch

--- patches/apply-patch.sh
+++ patches/apply-patch.sh
@@ -34,9 +34,9 @@ cd ../..
 cd packages/services/Telephony
 git apply -v ../../../device/elephone/p8000/patches/packages_services_telephony/NeedsFakeIccid.patch
 cd ../../..
-#cd system/core
-#git apply -v ../../device/elephone/p8000/patches/system_core/fix-boot-for-mtk.patch
-#cd ../..
+cd system/core
+git apply -v ../../device/elephone/p8000/patches/system_core/fix-boot-for-mtk.patch
+cd ../..
 cd system/netd
 git apply -v ../../device/elephone/p8000/patches/system_netd/hotpost-fix.patch
 cd ../..
